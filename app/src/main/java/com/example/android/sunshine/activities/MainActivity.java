f32aaa7de40ab2f1cade7f472cb765545cbf2903
EarvinJay <earvinjaybermudo21@gmail.com>
2016-01-28 12:09:49 +0800

    implemented adapter,list view(latest commit)

--- app/src/main/java/com/example/android/sunshine/activities/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/activities/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.android.sunshine.activities;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.android.sunshine.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState==null)
+        {
+
+
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -32,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        if(id==R.id.action_settings)
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
@@ -40,4 +55,31 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static class PlaceholderFragment extends Fragment {
+
+    public PlaceholderFragment()
+    {
+
+    }
+        @Override
+        public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState)
+
+        {
+
+            View rootview =inflater.inflate(R.layout.fragment_main,container);
+            String[] list=new String[]{"Today-Sunny-88/73","Tomorrow-Foggy-82/53","Saturday-Cloudy-768/60","Sunday-Sunny-73/63","Monday-Rainy-70/46","Tuesday-Sunny-78/43"};
+           ArrayAdapter ListAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview, list);
+            ListView listView = (ListView) rootview.findViewById(R.id.listView_forecast);
+            listView.setAdapter(ListAdapter);
+             return rootview;
+        }
+
+
+
+
+
+    }
+
+
 }
